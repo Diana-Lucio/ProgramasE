1a77295599058898187b0343774f31dd705e121e
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-01 17:19:57 -0600

    Create P 2-5.py

---
+++ codigos/P 2-5.py
@@ -0,0 +1,22 @@
+# Programa 2.5- Comparación, Membership, indexing, método de slicing
+# Fecha de elaboración: 2024/10/23   
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+
+# COMPARACIÓN
+print("\n")
+print("Perro" == "Perro") #True
+print("Perro" != "Gato") #True
+print("Aguascalientes" < "Zacatecas") #True
+print("Aire" >= "Agua") #True
+print("\n")
+# MEMBERSHIP
+print("house" in "boathouse") #True
+print("bien" in "bienvenidos") #True
+print("y" not in "ejes") #True
+print("je" in "ejes") #True
+print("\n")
+# SUBSCRIPTING
+nombre = "Diana Lucio"
+print(nombre [2:6])
+print("\n")
