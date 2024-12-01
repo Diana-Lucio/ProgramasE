329e6ab35b90d5497cdaa70b774499ace5c5d621
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-01 17:20:12 -0600

    Create P 2-6.py

---
+++ codigos/P 2-6.py
@@ -0,0 +1,12 @@
+# Programa 2.6- Negative subscript indices
+# Fecha de elaboración: 2024/10/24  
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+
+print("\n")
+word = "hamster"
+print(word[-1])
+print(word[1:-1])
+print(word[-3:])
+print(word[:3])
+print("\n")
