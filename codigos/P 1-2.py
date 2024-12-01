b53ea03968f6e4918ec4b47bb87bcdfccca42219
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-01 17:15:50 -0600

    Create P 1-2.py

---
+++ codigos/P 1-2.py
@@ -0,0 +1,6 @@
+# Programa 1.2- Programa que solicita el nombre de usuario y lo imprime con un mensaje personalizado 
+# Fecha de elaboración: 2024/10/14 
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+nombre = input("Ingresa tu nombre: ") # Método que permite ingresar texto desde teclado 
+print("Hola, " + nombre )  
