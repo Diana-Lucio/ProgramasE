da396c47d0d90770a85fa9b6107fcde6cf5dd4a7
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-03 07:17:44 -0600

    Create P 1-9.py

---
+++ codigos/P 1-9.py
@@ -0,0 +1,8 @@
+# Programa 1.8- Suma
+# Fecha de elaboración: 2024/10/17   
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+num = 39
+print(num)
+num = num + 1
+print(num)
