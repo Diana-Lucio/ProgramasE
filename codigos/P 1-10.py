b1633afc5d6e9ab9924e3e8815c93fdbb86cd4d2
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-03 21:48:52 -0600

    Create P 1-10.py

---
+++ codigos/P 1-10.py
@@ -0,0 +1,8 @@
+# Programa 1.10- Caracteres especiales
+# Fecha de elaboración: 2024/10/17   
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+print("Hola, \Estos son algunos de los caracteres especiales")
+# alt 165 para la ñ
+print("Estoy feliz \u263A\t\u2615\t\u2650")
+print("Alineando \t\u263A")
