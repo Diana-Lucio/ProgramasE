59e4f5d4b9ba464a8376af168885c3a46c0a90f8
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:37:03 -0600

    Create P 3-10.py

---
+++ codigos/bloque 3/P 3-10.py
@@ -0,0 +1,11 @@
+# Programa 3.10- Igualdad en listas. Compare con todas las demas listas 
+# Fecha de elaboración: 2024/10/31
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+nombres = ["Choto", "Emilio", "Luis"]
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("Jose" not in nombres) #True
+print("\n")
