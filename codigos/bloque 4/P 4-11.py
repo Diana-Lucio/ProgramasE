0273595d46b0fa77dcf1824302b996c05696ba60
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 23:08:06 -0600

    Create P 4-11.py

---
+++ codigos/bloque 4/P 4-11.py
@@ -0,0 +1,12 @@
+# Programa 4.11- Programa que se repite hasta que ingrese la palabra salir
+# Fecha de elaboración: 2024/10/09
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+print("\n")
+# Inicialización de variables
+palabra = ""
+while palabra != "salir":
+    palabra = input("Ingresa una palabra o 'salir' para terminar: ")
+    palabra = palabra.lower() # Convierte la palabra a minúsculas
+    print("Usted ingresó: ", palabra)
+print("\nFin del programa\n") 
