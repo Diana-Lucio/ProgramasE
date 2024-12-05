93033385d7d3edbd7591f7146ec279619ab4f291
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:18:23 -0600

    Create P 3-1.py

---
+++ codigos/bloque 3/P 3-1.py
@@ -0,0 +1,14 @@
+# Programa 3.1- Mascotas
+# Fecha de elaboración: 2024/10/28
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+mascota = input("¿Qué mascota tienes? ")
+if "perro" in mascota:
+    print("Es un perro, ¡que lindo!")
+elif "gato" in mascota:
+    print("Tienes un gato, ¡que adorable!")
+else:
+    print("Tipo de mascota desconocido...")
+print("Gracias por usar nuestro programa")
+print("\n")
