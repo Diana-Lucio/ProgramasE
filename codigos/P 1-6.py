92e7a197a0a547a60cbdcfa4f393591ef694ecc9
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:05:40 -0600

    Delete codigos/P 1-6.py

--- codigos/P 1-6.py
+++
@@ -1,8 +0,0 @@
-# Programa 1.6- Programa que calcula el área de un triángulo dados la base # y altura por el usuario 
-# Fecha de elaboración: 2024/10/14  
-# Elaborado por: Diana Lucio  
-#.................................................................................................. 
-base = float(input("Ingresa el valor de la base: ")) 
-altura = float(input("Ingresa el valor de la altura: ")) 
-área = base * altura / 2 
-print("El área del triángulo es: " + str(área)) 
