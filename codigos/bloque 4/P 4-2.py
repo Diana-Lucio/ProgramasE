6a7f6c74ac6473a1be4e557d062524f12f2a8058
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:57:14 -0600

    Create P 4-2.py

---
+++ codigos/bloque 4/P 4-2.py
@@ -0,0 +1,9 @@
+# Programa 4.2 introducción al Ciclo for
+# Fecha de elaboración: 2024/11/04
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+nombres = ["Luis", "Chuy", "Mauricio"]
+for nombre in nombres:
+    print("El nombre es: ", nombre)
+print("\n")
