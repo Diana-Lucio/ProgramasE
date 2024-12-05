158c272d478f4e05be3d753413e8ee4cc4f48619
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 23:04:18 -0600

    Create P 4-4.py

---
+++ codigos/bloque 4/P 4-4.py
@@ -0,0 +1,12 @@
+# Programa 4.4- Programa que imprima el nombre de 10 de mis plantas favoritas
+# Fecha de elaboración: 2024/11/04
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+plantas = ["Rosa","Lirio","Orquídea","Tulipán","Hortensia","Peonia","Familia","Lisianthus","Astromelia","Jazmín","Lavanda"]
+# Imprimir cada planta en la lista
+print("Mis 10 plantas favoritas son:")
+for plantas in plantas:
+    print(plantas)
+print("\nGracias por ver los 10 tipos de plantas favoritas de Diana Lucio")
+print("\n")
