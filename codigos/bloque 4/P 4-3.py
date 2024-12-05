77765480949a0e00ea7345dfd7796a44d8b7f48a
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:57:49 -0600

    Create P 4-3.py

---
+++ codigos/bloque 4/P 4-3.py
@@ -0,0 +1,25 @@
+# Programa 4.3 Funcion rango
+# Fecha de elaboración: 2024/11/04
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+print("Imprime los valores del 0 al 9")
+x = range(10)
+for num in x:
+    print(num)
+print("\n")
+print("Imprime los valores del 5 al 15")
+x1 = range(5, 16)
+for num in x1:
+    print(num)
+print("\n")
+print("Imprime los pares del 10 al 20")
+x2 = range(10, 21, 2)
+for num in x2:
+    print(num)
+print("\n")
+print("Imprime los impares del 11 al 21")
+x3 = range(11, 22, 2)
+for num in x3:
+    print(num)
+print("\n")
