498a04c24dec758834665e14bd39223f1d581341
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:25:16 -0600

    Create P 3-6.py

---
+++ codigos/bloque 3/P 3-6.py
@@ -0,0 +1,17 @@
+# Programa 3.6- Listas de secuencias de valores
+# Fecha de elaboración: 2024/10/30
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+print("Lista de enteros")
+print(["5, 8, 10"])
+
+print("\nLista de textos")
+print(["Manzana", "Piña", "Naranja"])
+
+print("\nLista de booleanos")
+print(["True, False, True"])
+
+print(("\nLista vacía"))
+print([])
+print("\n")
