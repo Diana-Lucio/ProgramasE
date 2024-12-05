ddbaa6f61b613c6a31d0c5275eea7ceb19416632
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:04:57 -0600

    Create P 1-5.py

---
+++ codigos/bloque 1/P 1-5.py
@@ -0,0 +1,14 @@
+# Programa 1.5- Programa que suma, resta, divide y miltipica 2 números  
+# Fecha de elaboración: 2024/10/14  
+# Elaborado por: Diana Lucio  
+#................................................................................................. 
+num1 = int(input("Ingresa el primer número: "))  
+num2 = int(input("Ingresa el segundo número: "))  
+suma = num1 + num2  
+resta = num1 - num2  
+multiplicación = num1 * num2  
+división = num1 / num2  
+print("El valor de la suma es: " + str(suma)) 
+print("El valor de la resta es: " + str(resta)) 
+print("El valor de la multiplicación es: " + str(multiplicación)) 
+print("El valor de la división es: " + str(división)) 
