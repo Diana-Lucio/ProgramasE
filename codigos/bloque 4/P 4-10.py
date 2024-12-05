0a9f39afd149dd93e7b8354209c07a10f3836d76
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 23:07:48 -0600

    Create P 4-10.py

---
+++ codigos/bloque 4/P 4-10.py
@@ -0,0 +1,15 @@
+# Programa 4.10- Programa para demostrar el funcionamiento de los métodos upper() y lower()
+# Fecha de elaboración: 2024/10/08
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+print("\n")
+# Definimos una cadena de texto
+cadena = "Python es un lenguaje de programación"
+print("\n", cadena)
+
+cadena_mayusculas = cadena.upper() # Convertimos la cadena a mayúscula
+print("\n", cadena_mayusculas)
+
+cadena_minusculas = cadena.lower() # Convertimos la cadena a minúsculas
+print("\n", cadena_minusculas)
+print("\nFin del programa\n")
