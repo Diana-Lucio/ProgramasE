9884aba6ed421444f668103c62dfd324a953f493
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:49:18 -0600

    Create P 3-12.py

---
+++ codigos/bloque 3/P 3-12.py
@@ -0,0 +1,17 @@
+# Programa 3.12- Método pop
+# Fecha de elaboración: 2024/10/31
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+útiles = ["Libreta", "Lápiz", "Borrador", "Pluma", "Corrector"]
+
+# Usamos pop para eliminar y devolver el último elemento
+ultimo_elemento = útiles.pop()
+print("Elemento eliminado:", ultimo_elemento)
+print("Lista después de usar pop:", útiles)
+
+# Usamos pop para eliminar y devolver el elemento en la posición 1
+elemento_1 = útiles.pop(1)
+print("Elemento eliminado en la posición 1:", elemento_1)
+print("Lista después de usar pop en la posición 1:", útiles)
+print("\n")
