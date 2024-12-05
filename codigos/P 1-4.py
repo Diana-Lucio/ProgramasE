dcd1dbd7dc8c2c7547fc784735275aca20eb797d
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:04:32 -0600

    Delete codigos/P 1-4.py

--- codigos/P 1-4.py
+++
@@ -1,9 +0,0 @@
-# Programa 1.4- Probar el funcionamiento del siguiente código 
-# Fecha de elaboración: 2024/10/14 
-# Elaborado por: Diana Lucio 
-#.................................................................................................. 
-print(1 + 2)       
-print(1 - 2)  
-print(1 * 2)    
-print("Concatenando cadenas " + "está bien") 
-# print("Concatenando cadenas " - " está mal") 
