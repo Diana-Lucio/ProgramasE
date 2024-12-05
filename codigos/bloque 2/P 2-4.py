1b97bcbe12efe33fc63d99a5721bade62092bf71
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:12:12 -0600

    Create P 2-4.py

---
+++ codigos/bloque 2/P 2-4.py
@@ -0,0 +1,10 @@
+# Programa 2.4- Programa que calcule los impuestos de un valor
+# Fecha de elaboración: 2024/10/23   
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+
+print("\n")
+salario = int(input("¿Cuál es tu salario? "))
+impuestos = salario * 0.16
+print("Los impuestos son " + str(impuestos) + " pesos" + "\n ¡Gracias por usar nuestro sistema!")
+print("\n")
