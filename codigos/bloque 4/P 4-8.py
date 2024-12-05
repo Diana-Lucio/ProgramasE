604a17a0bafe669088916613a900950f346b0edf
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 23:07:10 -0600

    Create P 4-8.py

---
+++ codigos/bloque 4/P 4-8.py
@@ -0,0 +1,23 @@
+# Programa 4.8- Las estructuras WHILE se ejecutan mientras las condiciones que se encuentra la verdadera
+# Fecha de elaboración: 2024/10/07
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+print("\n")
+# Ejemplo 1
+# Imprimir los números del 1 al 10
+i = 1 # Inicialización de la variable control
+while i <= 10: # Condición de parada
+    print(i)
+    i += 1 # Equivalente a i = i + 1
+# Sintaxis:
+# while <condición> 
+#   <cuerpo while>
+print("\n")
+
+# Ejemplo 2
+# Imprimir los números del 10 al 1
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+print("\n")
