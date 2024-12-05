23ed3e13b55f43222eae98b3761c0cbeb987a8af
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:03:55 -0600

    Delete codigos/P 1-3.py

--- codigos/P 1-3.py
+++
@@ -1,8 +0,0 @@
-# Programa 1.3- Programa que imprime el número de días de una semana,  
-# el nombre de una figura de 3 lados y equivalencia de 250cm en metros 
-# Fecha de elaboración: 2024/10/14 
-# Elaborado por: Diana Lucio 
-#.................................................................................................. 
-print(7)            # Entero o integer 
-print("Triángulo")  # Alfanumérico o string  
-print(0.25)         # Flotante o float 
