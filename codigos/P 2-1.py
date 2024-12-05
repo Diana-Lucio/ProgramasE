3fd36d530a991761338cfad0c6d0992b6fc1089b
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:09:02 -0600

    Delete codigos/P 2-1.py

--- codigos/P 2-1.py
+++
@@ -1,9 +0,0 @@
-# Programa 2.1- Programa que demuestre el funcionamiento compuerta NOT
-# Fecha de elaboración: 2024/10/22   
-# Elaborado por: Diana Lucio   
-#..................................................................................................  
-
-print("\n")
-print(not True) # Imprime false
-print(not False) # Imprime true
-print("\n")
