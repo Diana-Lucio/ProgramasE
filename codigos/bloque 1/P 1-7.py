7308898d3eb456fde0060ff34b8be00a9a4f98d9
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:06:04 -0600

    Create P 1-7.py

---
+++ codigos/bloque 1/P 1-7.py
@@ -0,0 +1,10 @@
+# Programa 1.7- Programa que calcula el área y el perímetro de un círculo, ingresando el valor 
+# del radio por el usuario 
+# Fecha de elaboración: 2024/10/14  
+# Elaborado por: Diana Lucio  
+#.................................................................................................. 
+radio = float(input("Ingresa el valor del radio: ")) 
+área = 3.1416 * radio ** 2 
+perímetro = 3.1416 * 2 * radio 
+print("El área del círculo es: " + str(área)) 
+print("El perímetro del círculo es: " + str(perímetro)) 
