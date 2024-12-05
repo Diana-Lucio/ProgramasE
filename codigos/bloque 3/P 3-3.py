11e77e843a94b144ee98dd28b52366e1f2020121
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:19:16 -0600

    Create P 3-3.py

---
+++ codigos/bloque 3/P 3-3.py
@@ -0,0 +1,14 @@
+# Programa 3.3- Identificación de tipos de datos str, int y float
+# Fecha de elaboración: 2024/10/28
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+variable = input("¿Cuál es tu edad? ")
+print(type(variable))
+
+variable = int(variable) # Se convierte la variable en tipo entero
+print(type(variable))
+
+variable = float(variable) # Se convierte la variable en tipo flotante
+print(type(variable))
+print("\n")
