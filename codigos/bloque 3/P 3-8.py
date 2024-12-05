22031ac4985cbe7a8d11521d9452bd8add399f68
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:30:54 -0600

    Create P 3-8.py

---
+++ codigos/bloque 3/P 3-8.py
@@ -0,0 +1,18 @@
+# Programa 3.8- Listas con elementos: String, Int, Booleano
+# Fecha de elaboración: 2024/10/31
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+print("Indexing")
+numeros = [10, 20, 30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+print(numeros[-1])
+
+print("\nSubscripting")
+nombres = ["Chorchis", "Choto", "Emiliano", "Pepe el toro"]
+print(nombres[1:])
+print(nombres[1:5])
+print(nombres[-2:])
+print("\n")
