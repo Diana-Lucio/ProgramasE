71748e266a30ea2d9b79993080fa6e1c03ca7ad5
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 23:11:53 -0600

    Update P 4-9.py

--- codigos/bloque 4/P 4-9.py
+++ codigos/bloque 4/P 4-9.py
@@ -1,5 +1,5 @@
 # Programa 4.9- Programa que demuestra el uso de los comandos \break\ y \continue\
-# Fecha de elaboración: 2024/10/08
+# Fecha de elaboración: 2024/11/15
 # Elaborado por: Diana Lucio   
 #..................................................................................................  
 print("\n")
