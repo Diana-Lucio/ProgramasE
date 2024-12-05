2f60f9cac0c0205cec4556dd3e3a7f5575808ca4
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:10:59 -0600

    Delete codigos/P 2-3.py

--- codigos/P 2-3.py
+++
@@ -1,8 +0,0 @@
-# Programa 2.3- Programa que comprueba el funcionamiento de adicion y concatenación de varias variables
-# Fecha de elaboración: 2024/10/22  
-# Elaborado por: Diana Lucio   
-#..................................................................................................  
-
-print("\n")
-print("Mi " + "nombre " + "es " + "Diana")
-print("\n")
