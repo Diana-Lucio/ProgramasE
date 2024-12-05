ee03753704b0fd7562a84365109080cf4e529997
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:33:16 -0600

    Create P 3-9.py

---
+++ codigos/bloque 3/P 3-9.py
@@ -0,0 +1,19 @@
+# Programa 3.8- Igualdad en listas
+# Fecha de elaboración: 2024/10/31
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+puntos = [10,30,20]
+puntos_2 = [10,30,20]
+ordenados = [10,20,30]
+puntos_textos = ["10","20","30"]
+
+print(puntos==puntos_2) 
+print(puntos==ordenados) 
+print(puntos=="puntos_textos") 
+print("\n")
+
+print(puntos!=puntos_2)
+print(puntos!=ordenados) 
+print(puntos!=puntos_textos) 
+print("\n")
