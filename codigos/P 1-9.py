7a1a0fc0ec05853dc02c4be5709660367d1476d5
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:07:11 -0600

    Delete codigos/P 1-9.py

--- codigos/P 1-9.py
+++
@@ -1,8 +0,0 @@
-# Programa 1.8- Suma
-# Fecha de elaboración: 2024/10/17   
-# Elaborado por: Diana Lucio   
-#..................................................................................................  
-num = 39
-print(num)
-num = num + 1
-print(num)
