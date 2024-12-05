915ca64b4d82079d138d050698b8725011049022
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:10:47 -0600

    Create P 2-3.py

---
+++ codigos/bloque 2/P 2-3.py
@@ -0,0 +1,8 @@
+# Programa 2.3- Programa que comprueba el funcionamiento de adicion y concatenación de varias variables
+# Fecha de elaboración: 2024/10/22  
+# Elaborado por: Diana Lucio   
+#..................................................................................................  
+
+print("\n")
+print("Mi " + "nombre " + "es " + "Diana")
+print("\n")
