7743879c06f3069da244f2daf23faf996bef8a72
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:40:00 -0600

    Create P 3-11.py

---
+++ codigos/bloque 3/P 3-11.py
@@ -0,0 +1,20 @@
+# Programa 3.11- Agregando elementos a una lista
+# Fecha de elaboración: 2024/10/31
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+print("Muestra como unir cadenas con .append une al color Verde ven la lista:")
+colores=["rojo", "azul"]
+print(colores)
+colores.append ("Verde")
+print(colores)
+
+#print(colores + "Amarillo") ESTO ES UN ERROR NO SE PUEDE CONCATENAR CADENAS DE TEXTO CON STR Y CADENA DE LISTA
+print("\n")
+
+print("Muestra como unir cadenas con .append une a Caramelos en la lista:")
+dulces =["Chicles", "Churros", "Gomitas"]
+print(dulces)
+dulces.append ("Caramelos")
+print(dulces)
+print("\n")
