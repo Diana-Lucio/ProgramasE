bbf761e21ddc471cb501c144ad43b070945f6ebd
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:02:17 -0600

    Update P 1-1.py

--- codigos/bloque 1/P 1-1.py
+++ codigos/bloque 1/P 1-1.py
@@ -1,1 +1,5 @@
-
+# Programa 1.1- Programa que imprime un mensaje de texto en pantalla 
+# Fecha de elaboración: 2024/10/14 
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("Hola, mi nombre es Diana")
