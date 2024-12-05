f595107698d29233bdbb989446766338b0ba1e02
Diana-Lucio <L241050165@pabellon.tecnm.mx>
2024-12-04 22:55:08 -0600

    Create P 4-1.py

---
+++ codigos/bloque 4/P 4-1.py
@@ -0,0 +1,17 @@
+# Programa 4.1 Ciclo for
+# Fecha de elaboración: 2024/11/04
+# Elaborado por: Diana Lucio 
+#.................................................................................................. 
+print("\n")
+lista_num = [10, 30, 40, 20, 35, 80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+# Haciendo lo mismo pero con un ciclo "for"
+for i in lista_num:
+    print(lista_num[1])
+print(i)
+print("\n")
